b834fa4aa64cc4c506d0ca8efda1feb15fbcbbee
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-13 23:45:22 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class solution {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    static void sort012(int a[], int arr_size)
+    {
+        int lo = 0;
+        int hi = arr_size - 1;
+        int mid = 0, temp = 0;
+        while (mid <= hi) {
+            switch (a[mid]) {
+                case 0: {
+                    temp = a[lo];
+                    a[lo] = a[mid];
+                    a[mid] = temp;
+                    lo++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                case 2: {
+                    temp = a[mid];
+                    a[mid] = a[hi];
+                    a[hi] = temp;
+                    hi--;
+                    break;
+                }
+            }
+        }
+    }
+    static void printArray(int arr[], int arr_size)
+    {
+        int i;
+        for (i = 0; i < arr_size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sort012(arr, n);
+        printArray(arr, n);
+    }
+}
